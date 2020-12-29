53803ff0dd4bc41fcdc087f9f33b24cdfa9c66d5
Jonathan Narvaez <jonaser06@gmail.com>
2020-12-29 01:40:21 -0500

    fix

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
- header('Access-Control-Allow-Origin: *'); 
- header("Access-Control-Allow-Credentials: true");
- header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
- header('Access-Control-Max-Age: 1000');
- header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json, charset=utf-8');
 /**
  * CodeIgniter
  *
