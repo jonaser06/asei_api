fc75a58c577ce8b822e9c4a2cfb2758f3dc9a3db
Jonathan Narvaez <jonaser06@gmail.com>
2020-12-29 02:24:59 -0500

    fix

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
 // $method = $_SERVER['REQUEST_METHOD'];
 // if($method == "OPTIONS") {
 // die();
