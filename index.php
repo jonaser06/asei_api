083df2df55160182c5081a3fbd2c1208f293f3ab
Jonathan Narvaez <jonaser06@gmail.com>
2020-12-29 01:34:54 -0500

    fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 
 header('Access-Control-Allow-Methods: GET, POST');
 
-header("Access-Control-Allow-Headers: X-Requested-With");
+header("Access-Control-Allow-Headers: X-Requested-With"); 
 /**
  * CodeIgniter
  *
