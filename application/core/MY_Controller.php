dbe3a3aca2f734666357a85942d5e3ad74183ec4
Jonathan Narvaez <jonaser06@gmail.com>
2020-12-30 02:40:58 -0500

    fix

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -54,7 +54,7 @@ class MY_Controller extends CI_Controller
         //         ->set_content_type('application/json')
         //         ->set_status_header($code)
         //         ->set_output(json_encode($this->data));
-        return json_encode($this->data);
+        echo json_encode($this->data);
     }
    
     private function body_data( bool $status, string $message, $data, $code ):array
