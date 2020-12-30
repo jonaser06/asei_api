a603969772b3c6d0e0015ec8ce3eb86e05c02225
renzo <renzoedward23@gmail.com>
2020-12-30 02:08:55 -0500

    add

--- application/controllers/api/LoginUser.php
+++ application/controllers/api/LoginUser.php
@@ -66,8 +66,7 @@ class LoginUser extends MY_Controller {
         
 	}
 	public function login() {
-        echo 'hola';
-        exit;
+        
         $_POST = $this->security->xss_clean($_POST);
         if(!array_key_exists('email',$_POST) || !array_key_exists('email',$_POST)) {
             return $this->output_json( 400 ,'email y password necesarios');
