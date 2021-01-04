061590c8a3f82c1189e47dfd7449354fb8d88ab0
renzo <renzoedward23@gmail.com>
2021-01-04 00:39:52 -0500

    a

--- application/models/Privileges_Model.php
+++ application/models/Privileges_Model.php
@@ -18,7 +18,7 @@ class Privileges_Model extends CI_Model
     }
     public function getAll()
     {
-        $this->db->select('p.TIPO as perfil, mod.nombre as módulo , CREAR as crear , ACTUALIZAR as actualizar , ELIMINAR as eliminar , VISUALIZAR as visualizar');
+        $this->db->select('p.TIPO as perfil, mod.nombre as modulo , CREAR as crear , ACTUALIZAR as actualizar , ELIMINAR as eliminar , VISUALIZAR as visualizar');
         $this->db->from(' modulos_perfiles  as mod_p');
         $this->db->join('perfiles as p', 'p.ID_PE = mod_p.ID_PE');
         $this->db->join('modulos as mod', 'mod.ID_MO = mod_p.ID_MO');
