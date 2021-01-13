a743bd06f87f4f5323db0260e674cadea9cba9da
renzo <renzoedward23@gmail.com>
2021-01-12 20:13:01 -0500

    add

--- application/models/FIle_Model.php
+++ application/models/FIle_Model.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class File_model extends CI_Model
+class File_Model extends CI_Model
 {
     protected $table = 'multimedia';
 
