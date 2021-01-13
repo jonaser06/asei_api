18f55a3fa5ab6d618fc7d78055a5279a281d8541
renzo <renzoedward23@gmail.com>
2021-01-12 20:09:41 -0500

    add correction

--- application/controllers/api/Notes.php
+++ application/controllers/api/Notes.php
@@ -7,7 +7,7 @@ class Notes extends MY_Controller {
     {
         parent::__construct();
         $this->load->model('Notes_Model', 'NotesModel');
-        $this->load->model('File_model','FileModel');
+        $this->load->model('File_Model','FileModel');
 
     }
     
