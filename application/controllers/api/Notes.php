77f97a0aee6e6532b2d822e72aa3e6f93cd80616
renzo <renzoedward23@gmail.com>
2021-01-16 19:01:59 -0500

    add

--- application/controllers/api/Notes.php
+++ application/controllers/api/Notes.php
@@ -94,11 +94,11 @@ class Notes extends MY_Controller {
 
         if($page > 1) {
             $prev = $page - 1  ;
-            $notes['prev'] = "$section?page=$prev&limit=$for_page";
+            $notes['prev'] = "/$section?page=$prev&limit=$for_page";
         } 
         if( $page < $pages ) {
             $next = $page + 1 ;
-            $notes['next'] = "$section?page=$next&limit=$for_page";
+            $notes['next'] = "/$section?page=$next&limit=$for_page";
         }
        
         $this->output_json( 200 , 'find notes for this section !' , $notes );
