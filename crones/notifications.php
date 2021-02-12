cace5ce641fb95bf3399e778f64e71a74c7f9f76
Jonathan Narvaez <jonaser06@gmail.com>
2021-02-11 23:01:35 -0500

    fix

--- crones/notifications.php
+++ crones/notifications.php
@@ -1,11 +1,11 @@
 <?php
 
     function sent_test(){
-        echo 'sent test';
-        $myfile = fopen("testfile.txt", "w");
-        if($myfile){
-            echo 'escrito';
-        }
+        echo 'sent testt';
+        var_dump( fopen("testfile.txt", "w"));
+        // if($myfile){
+        //     echo 'escrito';
+        // }
     }
     sent_test();
 ?>
