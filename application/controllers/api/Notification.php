3e6eae44e0e7064aa420a85c5fd0306a857e5e08
Jonathan Narvaez <jonaser06@gmail.com>
2021-02-15 23:38:42 -0500

    fix notification

--- application/controllers/api/Notification.php
+++ application/controllers/api/Notification.php
@@ -7,10 +7,13 @@ class Notification extends MY_Controller {
     {
 		parent::__construct();
 		$this->load->model('Notification_Model', 'notification');
+        date_default_timezone_set("America/Lima");
     }
 
     public function setNotification(){
-
+        // $date = new DateTime();
+        // echo $date->format("Y-m-d H:i:s");
+        // exit;
         if(($this->input->server('REQUEST_METHOD') === 'POST')){
             
             $inputJSON = file_get_contents('php://input');
@@ -23,7 +26,7 @@ class Notification extends MY_Controller {
             if ( isset($input['categoria']) && $input['categoria'] == "" ) return $this->output_json(400,'The category is necessary');
             if ( isset($input['ID_US']) && $input['ID_US'] == "" ) return $this->output_json(400,'The ID_US is necessary');
             if ( isset($input['estado']) && $input['estado'] == "" ) return $this->output_json(400,'The state is necessary'); # notificacion 1: Nuevo 2: Leido 3: Eliminado
-            
+
             $this->data[0] = [
                 'titulo'      => $input['titulo'],
                 'descripcion' => $input['descripcion'],
